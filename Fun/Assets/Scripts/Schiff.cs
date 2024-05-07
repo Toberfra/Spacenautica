d8c1968cb64322bda13e0f73b7514b90ba1d46e3
Toberfra <toberfra@gmx.de>
2024-05-07 18:49:10 +0200

    Revert "Aussenhaut eingfärbt"
    This reverts commit 72b008a00e7c8c4491e471bef25f650137b9b6a7.

--- Fun/Assets/Scripts/Schiff.cs
+++ Fun/Assets/Scripts/Schiff.cs
@@ -4,13 +4,10 @@ using UnityEngine;
 
 public class Schiff : MonoBehaviour
 {
- 
-    
-    
     // Start is called before the first frame update
     void Start()
     {
-        //rigidbodyDuese = this.GetComponent
+        
     }
 
     // Update is called once per frame
